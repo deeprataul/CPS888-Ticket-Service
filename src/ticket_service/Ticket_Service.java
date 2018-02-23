96481adc4d0e44e98348fea79efa95edeba0961d
Sean <Sean@Jaqen>
2018-02-23 17:06:23 -0500

    Formatting, update login command and add exit command

--- src/ticket_service/Ticket_Service.java
+++ src/ticket_service/Ticket_Service.java
@@ -26,28 +26,26 @@ public class Ticket_Service {
     public static void main(String[] args) {
         ArrayList<Account> accountsList = new ArrayList<>();
         Hashtable<String, Integer> accountsHash = new Hashtable<String, Integer>();
-        
+
         Account currentAccount = null;
         System.out.println("Ticket Selling System\n"
                 + "These are the valid input commands that you can enter. At "
                 + "any point, enter help to see this list again (commands are "
                 + "not case sensitive).\n\n" + helpString + "\n");
 
-        try(BufferedReader br = new BufferedReader(new FileReader("Current User Accounts.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("Current User Accounts.txt"))) {
             String line = br.readLine();
-            System.out.println(line);
-            if (!line.equals("END                         ")){
+            if (!line.equals("END                         ")) {
                 Account newAccount = new Account(line);
                 accountsList.add(newAccount);
                 accountsHash.put(newAccount.username, accountsList.size() - 1);
-            }
-            else{
+            } else {
                 br.close();
             }
         } catch (Exception ex) {
             System.err.println(ex.getMessage());
         }
-        
+
         Scanner scanner = new Scanner(System.in);
         String input = "Not Exit";
         while (!input.equals("exit")) {
@@ -56,36 +54,43 @@ public class Ticket_Service {
 
             switch (input) {
                 case "login":
-                    boolean attemptingLogin = true;
+                    if (currentAccount == null) {
+                        boolean attemptingLogin = true;
 
-                    while (attemptingLogin) {
-                        System.out.print("Enter your username: ");
-                        input = scanner.nextLine();
-                        System.out.print("Enter your password: ");
-                        String password = scanner.nextLine();
+                        while (attemptingLogin) {
+                            System.out.print("Enter your username: ");
+                            input = scanner.nextLine();
+                            System.out.print("Enter your password: ");
+                            String password = scanner.nextLine();
 
-                        if (accountsHash.containsKey(input) && 
-                                accountsList.get(accountsHash.get(input)).getPassword().equals(password)) {
-                            currentAccount = accountsList.get(accountsHash.get(input));
-                            attemptingLogin = false;
-                            System.out.println("Welcome " + currentAccount.getUsername());
-                        } else {
-                            boolean validInput = false;
-                            while (!validInput) {
-                                System.out.print("Invalid user credentials. Would you like to try again (y/n): ");
-                                input = scanner.nextLine().toLowerCase();
-                                switch (input) {
-                                    case "y":
-                                        validInput = true;
-                                        attemptingLogin = true;
-                                        break;
-                                    case "n":
-                                        validInput = true;
-                                        attemptingLogin = false;
-                                        break;
+                            if (accountsHash.containsKey(input)
+                                    && accountsList.get(accountsHash.get(input)).getPassword().equals(password)) {
+                                currentAccount = accountsList.get(accountsHash.get(input));
+                                attemptingLogin = false;
+                                System.out.println("Welcome " + currentAccount.getUsername());
+                            } else {
+                                boolean validInput = false;
+                                while (!validInput) {
+                                    System.out.print("Invalid user credentials. Would you like to try again (y/n): ");
+                                    input = scanner.nextLine().toLowerCase();
+                                    switch (input) {
+                                        case "y":
+                                            validInput = true;
+                                            attemptingLogin = true;
+                                            break;
+                                        case "n":
+                                            validInput = true;
+                                            attemptingLogin = false;
+                                            break;
+                                    }
                                 }
                             }
                         }
+                    }
+                    else{
+                        System.out.println("You are currently logged in as " + 
+                                currentAccount.getUsername() + ". Please " +
+                                "logout in order to login as a different user");
                     }
                     break;
                 case "logout":
@@ -105,11 +110,13 @@ public class Ticket_Service {
                 case "help":
                     System.out.println(helpString + "\n");
                     break;
+                case "exit":
+                    System.out.println("Good bye");
                 default:
-                    System.out.println("\nSorry. You have not entered a valid " + 
-                            "input. Please try again. If you would like to " +
-                            "see the list of commands, enter \"help\" " +
-                            "(without quotations)\n");
+                    System.out.println("\nSorry. You have not entered a valid "
+                            + "input. Please try again. If you would like to "
+                            + "see the list of commands, enter \"help\" "
+                            + "(without quotations)\n");
                     break;
             }
         }
