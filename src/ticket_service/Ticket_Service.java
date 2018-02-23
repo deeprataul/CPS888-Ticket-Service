8c2a0ed6ecfcb6f063897be1c2767856935dc092
Sean <>
2018-02-23 15:40:54 -0500

    Initial Commit

---
+++ src/ticket_service/Ticket_Service.java
@@ -0,0 +1,88 @@
+package ticket_service;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sean
+ */
+public class Ticket_Service {
+    static String helpString = "Login: Login to a specific user account\n"
+            + "Logout: Logout of the current account\nCreate: Create a new user"
+            + "account (can only be done by an admin)\nDelete: Remove "
+            + "a user account (can only be done by an admin)\nSell: "
+            + "Sell a ticket or tickets to an event\nBuy: Purchase a ticket or "
+            + "tickets to an event\nRefund: Issue a credit to a buyer from a "
+            + "seller's account\nAddcredit: Add credit to an account (can only "
+            + "be done by an admin)\nExit: Closes the application";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Account> accountsList = new ArrayList<>();
+        Hashtable<String, Integer> accountsHash = new Hashtable<String, Integer>();
+        
+        Account currentAccount = null;
+        System.out.println("Ticket Selling System\n"
+                + "These are the valid input commands that you can enter. At "
+                + "any point, enter help to see this list again (commands are "
+                + "not case sensitive).\n\n" + helpString + "\n");
+
+        try(BufferedReader br = new BufferedReader(new FileReader("Current User Accounts.txt"))) {
+            String line = br.readLine();
+            System.out.println(line);
+            if (!line.equals("END                         ")){
+                Account newAccount = new Account(line);
+                accountsList.add(newAccount);
+                accountsHash.put(newAccount.username, accountsList.size() - 1);
+            }
+            else{
+                br.close();
+            }
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        
+        Scanner scanner = new Scanner(System.in);
+        String input = "Not Exit";
+        while (!input.equals("exit")) {
+            System.out.print("Please enter a command: ");
+            input = scanner.nextLine().toLowerCase();
+
+            switch (input) {
+                case "login":
+                    System.out.print("Enter your username: ");
+                    input = scanner.nextLine();
+                    
+                    
+                    break;
+                case "logout":
+                    break;
+                case "create":
+                    break;
+                case "delete":
+                    break;
+                case "sell":
+                    break;
+                case "buy":
+                    break;
+                case "refund":
+                    break;
+                case "addcredit":
+                    break;
+                default:
+                    System.out.println("\nSorry. You have not entered a valid " + 
+                            "input. Please try again. If you would like to " +
+                            "see the list of commands, enter \"help\" " +
+                            "(without quotations)\n");
+                    break;
+            }
+        }
+    }
+
+}
